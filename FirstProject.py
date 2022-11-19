c851eb309347801098ce83e0a900d5d09e30f41b
Malek <malek.alsibai03@gmail.com>
2022-11-19 21:53:13 +0100

    new line character \n

--- FirstProject.py
+++ FirstProject.py
@@ -5,5 +5,5 @@
 # multi line string by having trip;e ""
 #print("""Hello World. My name is Malek. I am 19 years old.""")
 
-print("Hello World " + "My name is Malek " + "I am 19 years old") #concatenating
+print("Hello World. \n" + "My name is Malek. \n" + "I am 19 years old") #concatenating
 
