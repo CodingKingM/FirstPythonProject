f3114c0bfbd2be8c92b627874b542d1e58ae61e6
Malek <malek.alsibai03@gmail.com>
2022-11-19 22:33:22 +0100

    functions, variables and strings

--- FirstProject.py
+++ FirstProject.py
@@ -5,5 +5,25 @@
 # multi line string by having trip;e ""
 #print("""Hello World. My name is Malek. I am 19 years old.""")
 
-print("Hello World. \n" + "My name is Malek. \n" + "I am 19 years old") #concatenating
+#print("Hello World.\n" + "My name is Malek.\n" + "I am 19 years old") #concatenating
+#print("Hello World\n" * 100)
+
+#Lets build a coffe barista
+
+print ("Hello, Welcome to Malek Coffee")
+
+name = input ("What is your name ?\n")
+
+print ("Hello " + name + ", thank you so much for coming in today.")
+
+
+
+menu = "Black Coffe, Espresso, Latte, Hot chocolate"
+
+print (name + ", What would you like from our menu today? Here is what we are serving\n\n\n " + menu
+)
+
+order = input()
+
+print ("Sounds Good! " + name + ", we'll have " + order + " ready for you in a moment")
 
