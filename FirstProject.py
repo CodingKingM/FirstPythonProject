d56558af3ed36a850fe26c20b31242ff5d57d2bc
Malek <malek.alsibai03@gmail.com>
2022-11-19 21:50:34 +0100

    concatenating

--- FirstProject.py
+++ FirstProject.py
@@ -1,3 +1,9 @@
-print ("Hello World")
-print ('My name is Malek')
-print ("I am 19 years old")
+#print ("Hello World")
+#print ('My name is Malek')
+#print ("I am 19 years old")
+# we are practicing python (how to write comments)
+# multi line string by having trip;e ""
+#print("""Hello World. My name is Malek. I am 19 years old.""")
+
+print("Hello World " + "My name is Malek " + "I am 19 years old") #concatenating
+
