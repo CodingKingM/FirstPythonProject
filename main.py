95c75bbae30440c2afd927ad75d524ab48f5fe17
Malek <malek.alsibai03@gmail.com>
2022-11-20 12:41:43 +0100

    floating point number

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-name = "Malek"
-age = "19"
+name = "Malek"    #string (text)
+age = 19  # integers
+actual_age = 23.2003    #floating point number
+
+
 print (name)
 print (age)
 
+print(type(name))   
 print(type(age))
+
+print(type(actual_age))
